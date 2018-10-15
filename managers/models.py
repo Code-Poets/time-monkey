f23ccbddd9c94ff76e3b397539c3bee911daa011
Karrp <kogut0000@gmail.com>
2018-10-15 10:31:55 +0200

    Add Project model

--- managers/models.py
+++ managers/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+MAX_NAME_LENGTH = 128
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=MAX_NAME_LENGTH)
+    start_date = models.DateTimeField(auto_now_add=True)
+    stop_date = models.DateTimeField(null=True, blank=True, )
+    terminated = models.BooleanField(default=False)
+    # managers = models.ManyToManyField(CustomUser, on_delete=models.CASCADE)
+    # members = models.ManyToManyField(CustomUser, on_delete=models.CASCADE)
