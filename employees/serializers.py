01aba2afc4a69ed70ef465c3cc9cbe46f44a6159
Maciej <sam.maciej@interia.pl>
2018-10-16 17:05:12 +0200

    Add Report model serializer

---
+++ employees/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from .models import Report
+from managers.models import Project
+
+
+class ReportSerializer(serializers.HyperlinkedModelSerializer):
+
+    project = serializers.SlugRelatedField(
+        queryset=Project.objects.all(),
+        slug_field='name',
+    )
+    author = serializers.StringRelatedField()
+
+    class Meta:
+        model = Report
+        fields = (
+            'url',
+            'date',
+            'project',
+            'author',
+            'description',
+            'work_hours',
+        )
