b2b10061cd25d264b0cac9795f365c64608a31a0
Martyna <martyna.gottschling@gmail.com>
2018-10-25 13:11:39 +0200

    Remove hardcoded UserType value in users permissions

--- users/permissions.py
+++ users/permissions.py
@@ -1,10 +1,11 @@
 from rest_framework import permissions
+from users.models import CustomUser
 
 
 class IsAdminUser(permissions.BasePermission):
     message = 'You are not allowed to enter - only for admin.'
     def has_permission(self, request, view):
-        return request.user and request.user.is_authenticated and request.user.user_type == 'Admin'
+        return request.user and request.user.is_authenticated and request.user.user_type == CustomUser.UserType.ADMIN.name
 
 
 class IsOwnerOrAdmin(permissions.BasePermission):
@@ -12,10 +13,9 @@ class IsOwnerOrAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
         if view.get_object() == request.user:
             return True
-        return request.user and request.user.is_authenticated and request.user.user_type == 'Admin'
+        return request.user and request.user.is_authenticated and request.user.user_type == CustomUser.UserType.ADMIN.name
     def has_object_permission(self, request, view, obj):
-        if request.user:
-            if request.user.user_type == 'Admin':
+        if request.user.is_authenticated:
+            if request.user.user_type == CustomUser.UserType.ADMIN.name:
                 return True
             return obj == request.user
-        #return obj == request.user or request.user.user_type == 'Admin'
