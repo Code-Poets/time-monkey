2984d6d814b81844f7d8cd9353dd4bf207da6bbc
Karrp <kogut0000@gmail.com>
2018-11-13 15:17:24 +0100

    fixup! Add views for Project model

--- managers/views.py
+++ managers/views.py
@@ -1,6 +1,6 @@
+from rest_framework import viewsets
 from managers.models import Project
 from managers.serializers import ProjectSerializer
-from rest_framework import viewsets
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
