2e6d3a95b9f15e3d7b3600519cc7196e6d946364
Maciej <sam.maciej@interia.pl>
2018-11-14 15:57:09 +0100

    fixup! [M] Add Report model

--- employees/models.py
+++ employees/models.py
@@ -1,8 +1,9 @@
-from django.db import models
 from django.core.validators import MaxValueValidator
 from django.core.validators import MinValueValidator
+from django.db import models
 
 from employees.common.constants import ReportModelConstants
+from employees.common.validators import MaxDecimalValueValidator
 from managers.models import Project
 from users.models import CustomUser
 
@@ -10,7 +11,7 @@ from users.models import CustomUser
 class Report(models.Model):
     date = models.DateField()
     description = models.CharField(
-        max_length=ReportModelConstants.MAX_DESCRIPTION_LENGTH,
+        max_length=ReportModelConstants.MAX_DESCRIPTION_LENGTH.value,
     )
     creation_date = models.DateTimeField(
         auto_now_add=True,
@@ -27,13 +28,13 @@ class Report(models.Model):
         on_delete=models.CASCADE,
     )
     work_hours = models.DecimalField(
-        max_digits=ReportModelConstants.MAX_DIGITS,
-        decimal_places=ReportModelConstants.DECIMAL_PLACES,
+        max_digits=ReportModelConstants.MAX_DIGITS.value,
+        decimal_places=ReportModelConstants.DECIMAL_PLACES.value,
         validators=
         [
-            MinValueValidator(ReportModelConstants.MIN_WORK_HOURS),
-            MaxValueValidator(ReportModelConstants.MAX_WORK_HOURS),
-            MaxDecimalValueValidator(ReportModelConstants.MAX_WORK_HOURS_DECIMAL_VALUE),
+            MinValueValidator(ReportModelConstants.MIN_WORK_HOURS.value),
+            MaxValueValidator(ReportModelConstants.MAX_WORK_HOURS.value),
+            MaxDecimalValueValidator(ReportModelConstants.MAX_WORK_HOURS_DECIMAL_VALUE.value),
         ]
     )
     editable = models.BooleanField(
