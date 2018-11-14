b8fd951f9f9a354d953be4eaef2030f1f14dc078
Maciej <sam.maciej@interia.pl>
2018-11-14 14:21:20 +0100

    fixup! Add constants for employees app
    To match with previously released users app

--- employees/common/constants.py
+++ employees/common/constants.py
@@ -1,7 +1,5 @@
 from decimal import Decimal
 
-from django.utils.translation import ugettext_lazy
-
 
 class ReportModelConstants:
     MAX_DESCRIPTION_LENGTH = 256
@@ -10,4 +8,3 @@ class ReportModelConstants:
     MAX_WORK_HOURS = Decimal('24.00')
     MIN_WORK_HOURS = Decimal('0.01')
     MAX_WORK_HOURS_DECIMAL_VALUE = Decimal('0.59')
-    MAX_DECIMAL_VALUE_VALIDATOR_MESSAGE = ugettext_lazy('Ensure this value is less than or equal to ')
