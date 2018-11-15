9b6a861f80e425765678ea2cad38ff20b0d47916
Martyna <martyna.gottschling@gmail.com>
2018-11-15 12:36:30 +0100

    fixup! Add DRF user registration

--- time_monkey/urls.py
+++ time_monkey/urls.py
@@ -12,7 +12,8 @@ Class-based views
 """
 from django.conf.urls import url
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include
+from django.urls import path
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
