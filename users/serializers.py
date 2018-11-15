3ad25ef009ae75a0c7ed3dd8e634b0bb903de2e4
Martyna <martyna.gottschling@gmail.com>
2018-11-15 13:05:58 +0100

    fixup! Add DRF user registration

--- users/serializers.py
+++ users/serializers.py
@@ -1,8 +1,15 @@
+from allauth.account import app_settings as allauth_settings
+from allauth.account.adapter import get_adapter
+from allauth.account.utils import setup_user_email
+from allauth.utils import email_address_exists
+
 from django_countries.serializers import CountryFieldMixin
 from django_countries.serializer_fields import CountryField
-from rest_framework import serializers
-from users.models import CustomUser
+from django.utils.translation import ugettext_lazy as _
 
+from rest_framework import serializers
+from users.common.strings import CustomValidationErrorText
+from users.models import CustomUser
 
 
 class UserSerializer(CountryFieldMixin, serializers.ModelSerializer):
@@ -68,41 +75,35 @@ class UserCreateSerializer(UserSerializer):
         )
 
 
-from allauth.account import app_settings as allauth_settings
-from allauth.utils import email_address_exists
-from allauth.account.adapter import get_adapter
-from allauth.account.utils import setup_user_email
-from django.utils.translation import ugettext_lazy as _
-
 class CustomRegisterSerializer(serializers.Serializer):
     email = serializers.EmailField(required=allauth_settings.EMAIL_REQUIRED)
     first_name = serializers.CharField(required=False, write_only=True)
     last_name = serializers.CharField(required=False, write_only=True)
-    password1 = serializers.CharField(required=True, write_only=True)
-    password2 = serializers.CharField(required=True, write_only=True)
+    password = serializers.CharField(required=True, write_only=True)
+    password_confirmation = serializers.CharField(required=True, write_only=True)
 
     def validate_email(self, email):
         email = get_adapter().clean_email(email)
         if allauth_settings.UNIQUE_EMAIL:
             if email and email_address_exists(email):
                 raise serializers.ValidationError(
-                    _("A user is already registered with this e-mail address."))
+                    CustomValidationErrorText.VALIDATION_ERROR_SIGNUP_EMAIL_MESSAGE)
         return email
 
-    def validate_password1(self, password):
+    def validate_password(self, password):
         return get_adapter().clean_password(password)
 
     def validate(self, data):
-        if data['password1'] != data['password2']:
+        if data['password'] != data['password_confirmation']:
             raise serializers.ValidationError(
-                _("The two password fields didn't match."))
+                CustomValidationErrorText.VALIDATION_ERROR_SIGNUP_PASSWORD_MESSAGE)
         return data
 
     def get_cleaned_data(self):
         return {
             'first_name': self.validated_data.get('first_name', ''),
             'last_name': self.validated_data.get('last_name', ''),
-            'password1': self.validated_data.get('password1', ''),
+            'password': self.validated_data.get('password', ''),
             'email': self.validated_data.get('email', ''),
         }
 
