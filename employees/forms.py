50bf6c59305671e6818aad99dd1f98fe04c61484
Maciej <sam.maciej@interia.pl>
2019-02-12 15:40:11 +0100

    fixup! Add form for project joining feature

--- employees/forms.py
+++ employees/forms.py
@@ -1,9 +1,6 @@
 from django import forms
 from django.db.models import QuerySet
 
-from managers.models import Project
-from users.models import CustomUser
-
 
 class ProjectJoinForm(forms.Form):
 
@@ -12,5 +9,4 @@ class ProjectJoinForm(forms.Form):
     def __init__(self, queryset, *args, **kwargs):
         super().__init__(*args, **kwargs)
         assert isinstance(queryset, QuerySet)
-        # assert isinstance(queryset.model, Project)      No exception message supplied
         self.fields['projects'].choices = [(project.id, project.name) for project in queryset]
