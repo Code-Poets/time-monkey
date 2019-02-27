20a1bb6fc3adb3b41ca1fec1a4e467364df60c18
Szymi1994 <mszymczak_94@wp.pl>
2019-02-27 11:49:29 +0100

    Create LoginAuthentication and override parent class clean method

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth import authenticate
+from django.contrib.auth.forms import UserChangeForm, AuthenticationForm
 from django.contrib.auth.forms import UserCreationForm
 from django_countries.widgets import CountrySelectWidget
 from users.models import CustomUser
@@ -24,3 +25,18 @@ class CustomUserChangeForm(UserChangeForm):
         model = CustomUser
         fields = '__all__'
         widgets = {'country': CountrySelectWidget()}
+
+
+class LoginAuthentication(AuthenticationForm):
+    def clean(self):
+        username = self.cleaned_data.get('username')
+        if "@" not in str(username):
+            username = str(username) + "@codepoets.it"
+        password = self.cleaned_data.get('password')
+
+        if username is not None and password:
+            self.user_cache = authenticate(self.request, username=username, password=password)
+            if self.user_cache is None:
+                raise self.get_invalid_login_error()
+            else:
+                self.confirm_login_allowed(self.user_cache)
