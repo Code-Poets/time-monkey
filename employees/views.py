4c5d09cfef80a8a7880b22c8d5332c213914d683
Maciej <sam.maciej@interia.pl>
2019-05-10 16:24:49 +0200

    fixup! Add username grouping in project list view

--- employees/views.py
+++ employees/views.py
@@ -230,7 +230,11 @@ class ProjectReportList(ListView):
     model = Report
 
     def get_queryset(self) -> QuerySet:
-        return Report.objects.filter(project__id=self.kwargs["pk"]).order_by("author__email", "-date")
+        return (
+            Report.objects.select_related("author")
+            .filter(project__id=self.kwargs["pk"])
+            .order_by("author__email", "-date")
+        )
 
     def get_context_data(self, **kwargs: Any) -> dict:
         context = super().get_context_data(**kwargs)
