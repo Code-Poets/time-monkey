c8bc51e6f6fae3aebbdbcdc6f953fb6bff00097b
Maciej <sam.maciej@interia.pl>
2020-05-18 11:07:40 +0200

    fixup! Add function for formatting time delta

--- utils/helpers.py
+++
@@ -1,7 +0,0 @@
-import datetime
-
-
-def format_timedelta_to_hours_and_minutes_only(timedelta: datetime.timedelta) -> str:
-    minutes = (timedelta.seconds + timedelta.days * 86400) // 60
-    hours, minutes = divmod(minutes, 60)
-    return "{:d}:{:02d}".format(hours, minutes)
